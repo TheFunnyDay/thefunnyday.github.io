7353ea982132dcfa46c14565c92d498634fc0a92
Nikita Golubev <39840774+TheFunnyDay@users.noreply.github.com>
2020-11-10 20:39:34 +0500

    g

---
+++ works/SB/sb-redesign/this_is_404_bro.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html style="font-family: 'Press Start 2P', cursive;
+">
+<head>
+	<title>404</title>
+<link href="https://fonts.googleapis.com/css?family=Press+Start+2P" rel="stylesheet">
+<audio src="video/127381293.mp3" autoplay>
+	
+</audio>
+</head>
+
+<body style="background-color: #EFEFEF;">
+
+	<center>
+ <img src="https://media.giphy.com/media/A9EcBzd6t8DZe/giphy.gif">
+ <h2>...page not found...</h2>
+ <a href="index.php"><h2>back</h2></a>
+ </center>
+</body>
+</html>
